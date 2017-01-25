f4085f7c70e6e8996a83ed7ab173b8c6525f62ae
RocknRick <s0554601@htw-berlin.de>
2017-01-25 18:56:46 +0100

    Listings

---
+++ Labor2/2.3.embeddedCode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+############################################################
+# Code fuer ein C-Programm wird ueber das Skript           #
+# an den gcc uebergeben und compiliert.                    #
+# Im Anschluss wird das Programm im Hintergrund gestartet  #
+# und mittels der PID und kill beendet                     #
+############################################################
+
+############################################################
+# Uebergabe des Codes an den gcc
+gcc -o ./c_file -x c - << EOF
+#include <stdio.h>
+int main()
+{
+while(1);
+return 0;
+}
+EOF
+
+
+# Das C-Programm wird im Hintergrund gestartet
+./c_file&
+echo "Programm läuft"
+ps
+
+# Das Programm wird ueber seine PID mit kill beendet
+kill $(ps -C c_file -o pid=)
+echo "Programm läuft nicht"
+ps
+
+ 
